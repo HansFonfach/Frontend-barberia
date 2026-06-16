8e34df5a703c95a5c8e6338048f9808e83cff468
hansfonfach <hans.fonfach@gmail.com>
2026-06-16 11:31:37 -0400

    fix: empresa.rubro

--- src/components/Headers/UserHeader.js
+++ src/components/Headers/UserHeader.js
@@ -11,7 +11,7 @@ const UserHeader = () => {
 
   if (empresa.tipo === "barberia") {
     backgroundImg = require("../../assets/img/theme/profile-cover.jpg");
-  } else if (empresa.tipo === "salon_belleza") {
+  } else if (empresa.tipo || empresa.rubro === "salon_belleza") {
     backgroundImg = require("../../assets/img/theme/lifting-pestanas.png");
   } else {
     // Imagen por defecto
